f2499154355a361e74e0d58b6210a48c19faa887
RiderLty <135112135+RiderLty@users.noreply.github.com>
2025-09-20 22:05:50 +0800

    触摸点max修改为9，修复某些机型无法创建虚拟触屏的bug

--- u_input_interface.go
+++ u_input_interface.go
@@ -67,7 +67,7 @@ func create_u_input_touch_screen(width int32, height int32) *os.File {
 	absMax[absMtPositionY] = height << touch_pos_scale //坐标系会自动以原点缩放
 	absMax[absMtTouchMajor] = 255
 	absMax[absMtWidthMajor] = 0
-	absMax[absMtSlot] = 10
+	absMax[absMtSlot] = 9
 	absMax[absMtTrackingId] = 65535
 
 	uiDev := UinputUserDev{
