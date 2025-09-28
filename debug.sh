3029eee8746d035420c15bbbe4f997bc19058ad4
RiderLty <lty6531600@gmail.com>
2025-09-28 20:23:45 +0800

    添加README

--- debug.sh
+++ debug.sh
@@ -2,5 +2,5 @@ CGO_ENABLE=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o go-touch-mappe
 adb push ./go-touch-mapper /data/local/tmp
 adb push ./configs/EXAMPLE.JSON /data/local/tmp
 # adb shell /data/local/tmp/go-touch-mapper -d -r -c /data/local/tmp/EXAMPLE.JSON 
-adb shell /data/local/tmp/go-touch-mapper -d -r # | grep "sendEvents write" 
+adb shell /data/local/tmp/go-touch-mapper -d -r -v
 
