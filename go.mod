efee5506f3f06658c1891162c8298ea7218c19ca
RiderLty <lty6531600@gmail.com>
2026-06-17 14:14:44 +0800

    refactor(otg hid): 重构触摸管理实现，更新依赖并添加默认配置
    移除对外部linux-cu库的依赖，改用直接读写/dev/hidg0设备节点；新增默认触摸映射插件配置文件；整理并更新go.mod与go.sum依赖包；调整触摸报告缓冲区结构与写入偏移

--- go.mod
+++ go.mod
@@ -7,16 +7,20 @@ toolchain go1.24.4
 // go 1.18
 
 require (
-	github.com/RiderLty/linux-cu v0.0.0-20260510041912-0072fc3a65e1 // indirect
-	github.com/akamensky/argparse v1.3.1 // indirect
-	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/akamensky/argparse v1.3.1
+	github.com/bitly/go-simplejson v0.5.0
+	github.com/kenshaw/evdev v0.1.0
+	github.com/lunixbochs/struc v0.0.0-20200707160740-784aaebc1d40
+	github.com/withmandala/go-log v0.1.0
+	go.bug.st/serial v1.6.4
+	golang.org/x/sys v0.19.0
+)
+
+require (
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/creack/goselect v0.1.2 // indirect
-	github.com/kenshaw/evdev v0.1.0 // indirect
-	github.com/lunixbochs/struc v0.0.0-20200707160740-784aaebc1d40 // indirect
-	github.com/withmandala/go-log v0.1.0 // indirect
-	go.bug.st/serial v1.6.4 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/smartystreets/goconvey v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
-	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- otg_hid_manager.go
+++ otg_hid_manager.go
@@ -3,67 +3,36 @@ package main
 import (
 	"encoding/binary"
 	"os"
-
-	"github.com/RiderLty/linux-cu/pkg/emulate"
 )
 
 func handel_touch_using_otg_manager() touch_control_func {
-	yamlData := []byte(`device:
-    bcdUSB: "0x0200"
-    bDeviceClass: 0
-    bDeviceSubClass: 0
-    bDeviceProtocol: 0
-    bMaxPacketSize0: 64
-    idVendor: "0x054D"
-    idProduct: "0x0ce7"
-    bcdDevice: "0x0101"
-    manufacturer: Sony Interactive Entertainment
-    product: Touch Screen Controller
-configs:
-    - bConfigurationValue: 1
-      MaxPower: 50
-      selfPowered: false
-      remoteWakeup: true
-      bNumInterfaces: 1
-      interfaces:
-        - bInterfaceNumber: 0
-          bAlternateSetting: 0
-          bInterfaceClass: 3
-          bInterfaceSubClass: 0
-          bInterfaceProtocol: 0
-          interfaceString: Touch Screen
-          endpoints:
-            - bEndpointAddress: 132
-              bmAttributes: 3
-              wMaxPacketSize: 64
-              bInterval: 1
-          reportDescriptor: 050d0904a1010922a10209421500250175019501810295078103095175089501810205010930150027feffff7f7520950181020931150027feffff7f752095018102c0050d09542502750895018102c0
-`)
-	g, err := emulate.Start(yamlData)
+	touch_fd, err := os.OpenFile("/dev/hidg0", os.O_RDWR, 0666)
 	if err != nil {
-		logger.Errorf("无法创建 USB HID Gadget: %s", err.Error())
+		logger.Errorf("无法打开OTG HID设备文件: %s", err.Error())
 		os.Exit(4)
 	}
+	// mouse_fd, err := os.OpenFile("/dev/hidg1", os.O_RDWR, 0666)
+	// if err != nil {
+	// 	logger.Errorf("无法打开OTG HID设备文件: %s", err.Error())
+	// 	os.Exit(4)
+	// }
+	// keyboard_fd, err := os.OpenFile("/dev/hidg2", os.O_RDWR, 0666)
+	// if err != nil {
+	// 	logger.Errorf("无法打开OTG HID设备文件: %s", err.Error())
+	// 	os.Exit(4)
+	// }
+	// defer touch_fd.Close()
+	// defer mouse_fd.Close()
+	// defer keyboard_fd.Close()
 
-	go func() {
-		<-global_close_signal
-		g.Close()
-	}()
-
-	// Report layout (matching esp32.yaml descriptor):
-	// [0]    Tip Switch (1bit) + padding (7bit) = 1 byte
-	// [1]    Contact ID = 1 byte
-	// [2:6]  X = 4 bytes (LE)
-	// [6:10] Y = 4 bytes (LE)
-	// [10]   Contact Count = 1 byte
-	var buf [11]byte
-
+	var buf [12]byte
+	buf[0] = 0x01
 	setReport := func(action uint8, id uint8, x, y uint32) {
-		buf[0] = action
-		buf[1] = id
-		binary.LittleEndian.PutUint32(buf[2:6], x)
-		binary.LittleEndian.PutUint32(buf[6:10], y)
-		buf[10] = 1 // contact count
+		buf[1] = action
+		buf[2] = id
+		binary.LittleEndian.PutUint32(buf[3:7], x)
+		binary.LittleEndian.PutUint32(buf[7:11], y)
+		buf[11] = 0
 	}
 
 	return func(control_data touch_control_pack) {
@@ -71,10 +40,10 @@ configs:
 		case TouchActionRequire, TouchActionMove:
 			x, y := rotateAbsoluteXY(control_data.x, control_data.y)
 			setReport(0x01, uint8(control_data.id), uint32(x), uint32(y))
-			g.Write(buf[:])
+			touch_fd.Write(buf[:])
 		case TouchActionRelease:
 			setReport(0x00, uint8(control_data.id), 0, 0)
-			g.Write(buf[:])
+			touch_fd.Write(buf[:])
 		}
 	}
 }
